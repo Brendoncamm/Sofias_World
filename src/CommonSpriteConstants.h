2cdc9a67cc889f730eef56d2208211320552fd9f
Brendoncamm <55641029+unlmted@users.noreply.github.com>
2025-04-07 21:39:01 -0300

    Sprite constants namespace

---
+++ src/CommonSpriteConstants.h
@@ -0,0 +1,13 @@
+#include <SFML/Graphics.hpp>
+
+namespace CommonSpriteConstants
+{
+    int FRAME_WIDTH_32_BIT  = 32;
+    int FRAME_HEIGHT_32_BIT = 32;
+    int FRAME_WIDTH_64_BIT  = 64;
+    int FRAME_HEIGHT_64_BIT = 64;
+
+    sf::Vector2i SPRITE_SHEET_OFFSET = {0, 0};
+    sf::Vector2f REFLECT_LEFT        = {-5.f, 5.f};
+    sf::Vector2f REFLECT_RIGHT       = {5.f, 5.f};
+};
