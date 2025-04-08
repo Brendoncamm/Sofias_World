721409b3b6c16b4cfb335fa4d32b20e7884df182
Brendoncamm <55641029+unlmted@users.noreply.github.com>
2025-04-08 08:53:19 -0300

    Added static_assert to template function to save headache later

--- src/CommonSpriteUtilities.h
+++ src/CommonSpriteUtilities.h
@@ -8,8 +8,10 @@ namespace CommonSpriteUtilities
 {
 
     template<int PixelPerUnit>
-    sf::IntRect getSpriteFrameRect(int currentFrame)
+    inline sf::IntRect getSpriteFrameRect(int currentFrame)
     {
+        static_assert((PixelPerUnit > 0) && (PixelPerUnit <= 128), "PixelPerUnit must be greater than 0");
+        
         int x = CommonSpriteConstants::SHEET_OFFSET.x + (PixelPerUnit * currentFrame);
         int y = CommonSpriteConstants::SHEET_OFFSET.y;
         return sf::IntRect(sf::Vector2i(x, y), sf::Vector2i(PixelPerUnit, PixelPerUnit));
